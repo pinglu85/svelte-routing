699636e5fe6f8adf7c67fae29966a527955dfbe8
pinglu85 <luping8576@hotmail.com>
2022-12-11 17:14:36 +0100

    first commit

---
+++ src/lib/routing/index.ts
@@ -0,0 +1,47 @@
+import type { SvelteComponent } from 'svelte';
+
+import NotFound from './NotFound.svelte';
+
+interface Route {
+  url: string;
+  component: typeof SvelteComponent;
+}
+
+export function createRouting({
+  routes,
+  target,
+}: {
+  routes: Route[];
+  target: HTMLElement;
+}) {
+  let currComponent: SvelteComponent | undefined;
+  const pathname = location.pathname;
+  matchRoute(pathname);
+
+  function matchRoute(pathname) {
+    if (currComponent) currComponent.$destroy();
+
+    const matchedRoute = routes.find((route) => route.url === pathname);
+    const MatchedComponent = matchedRoute?.component ?? NotFound;
+    currComponent = new MatchedComponent({
+      target,
+    });
+
+    document.querySelectorAll('a').forEach((a) => {
+      a.addEventListener('click', function (e) {
+        if (a.target) return;
+
+        e.preventDefault();
+
+        const targetLocation = this.href;
+        const targetPathname = new URL(targetLocation).pathname;
+
+        // Update the URL without navigating
+        history.pushState({}, undefined, targetPathname);
+
+        // Match the component and render new content
+        matchRoute(targetPathname);
+      });
+    });
+  }
+}
---
+++ src/routes/A.svelte
@@ -0,0 +1,3 @@
+<h1>A</h1>
+
+<a href="/b">Go to B</a>
