8c144a96033ced52d5a31d4b7742d2821b14a23c
pinglu85 <luping8576@hotmail.com>
2022-12-21 14:43:54 +0100

    ignore typescript error

--- src/lib/routing/Testing.svelte
+++ src/lib/routing/Testing.svelte
@@ -1,10 +1,12 @@
 <button
   on:click={() => {
+    // @ts-ignore
     window.showLoadingIndicator();
   }}>Show</button
 >
 <button
   on:click={() => {
+    // @ts-ignore
     window.hideLoadingIndicator();
   }}>Hide</button
 >
