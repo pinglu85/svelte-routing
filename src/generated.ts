2fbf3421c4d93599b322a8d5dde2b9fca26c7893
pinglu85 <luping8576@hotmail.com>
2022-12-22 16:11:10 +0100

    extract routes from files

--- src/generated.ts
+++ src/generated.ts
@@ -1,1 +1,47 @@
-console.log("hello");
+import { createRouting } from './lib/routing';
+
+createRouting({
+  routes: [
+    {
+      url: /^\/a\/[...rest]\/?$/,
+      params: [],
+      components: [() => import('./$/a/[...rest].svelte')],
+    },
+    {
+      url: /^\/b\/?$/,
+      params: [],
+      components: [() => import('./$/b.svelte')],
+    },
+    {
+      url: /^\/c\/?$/,
+      params: [],
+      components: [() => import('./$/c.svelte')],
+    },
+    {
+      url: /^\/\/?$/,
+      params: [],
+      components: [() => import('./$/index.svelte')],
+    },
+    {
+      url: /^\/item\/[shopId]\/[itemId]\/?$/,
+      params: [],
+      components: [() => import('./$/item/[shopId]/[itemId].svelte')],
+    },
+    {
+      url: /^\/item\/[shopId]\/__layout\/?$/,
+      params: [],
+      components: [() => import('./$/item/[shopId]/__layout.svelte')],
+    },
+    {
+      url: /^\/shop\/[shopId]\/?$/,
+      params: [],
+      components: [() => import('./$/shop/[shopId].svelte')],
+    },
+    {
+      url: /^\/shop\/__layout\/?$/,
+      params: [],
+      components: [() => import('./$/shop/__layout.svelte')],
+    },
+  ],
+  target: document.getElementById('app'),
+});
--- src/lib/routing/script/generate-routes.js
+++ src/lib/routing/script/generate-routes.js
@@ -5,16 +5,66 @@ const cwd = process.cwd();
 
 const outputFilePath = path.join(cwd, 'src/generated.ts');
 const inputFolder = path.join(cwd, 'src/$');
+const routes = exploreFolders(inputFolder);
 
 fs.writeFileSync(
   outputFilePath,
+  // We need to write `routes: [ ${routes.map(...)} ]` instead of
+  // `routes: ${routes.map(...)}`, because without `[ ]` we'll get
+  // `routes: {}, {}, ...` in the `generated.ts` file. The reason for
+  // that is the values that are interpolated into a template literal
+  // are converted to their string representation. For objects and
+  // arrays, that means calling their `.toString()` method.
   `
     import { createRouting } from './lib/routing';
 
     createRouting({
-      routes: [],
+      routes: [
+        ${routes
+          .map(({ componentPath, relativePath }) => {
+            const component = relativePath.replace(/\.svelte$/, '');
+            const segments = component.split('/');
+            if (segments[segments.length - 1] === 'index') {
+              segments[segments.length - 1] = '';
+            }
+
+            const regExp = `/^\\/${segments.join('\\/')}\\/?$/`;
+
+            return `{
+            url: ${regExp},
+            params: [],
+            components: [() => import('./$/${relativePath}')],
+          }`;
+          })
+          .join(',\n')}
+      ],
       target: document.getElementById('app'),
     });
   `,
   'utf-8'
 );
+
+function exploreFolders(rootRouteDirectory) {
+  const routes = [];
+
+  function _explore(folderPath) {
+    const files = fs.readdirSync(folderPath);
+
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+      const isDirectory = fs.statSync(filePath).isDirectory();
+
+      if (isDirectory) {
+        _explore(filePath);
+      } else {
+        routes.push({
+          componentPath: filePath,
+          relativePath: path.relative(rootRouteDirectory, filePath),
+        });
+      }
+    }
+  }
+  _explore(rootRouteDirectory);
+
+  return routes;
+}
